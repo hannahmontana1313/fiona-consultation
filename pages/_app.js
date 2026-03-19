5d30ab4f15ed8609beb49859cbf3da318ab7f97b
hannahmontana1313 <caruso.fiona@icloud.com>
2026-03-19 23:27:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
 import { AuthProvider } from '../components/AuthContext';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <Footer />
     </AuthProvider>
   );
 }
