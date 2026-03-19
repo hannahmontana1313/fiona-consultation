65e27a4ee32347378f281f8c3b9fee8805b33d75
Fiona Caruso <fionacaruso@air-de-fiona.home>
2026-03-19 05:44:23 +0100

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Permettre le body parser désactivé pour le webhook Stripe
-  api: {
-    bodyParser: false,
-  },
 };
 
 module.exports = nextConfig;
